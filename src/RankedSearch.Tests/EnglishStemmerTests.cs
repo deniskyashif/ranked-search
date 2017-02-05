4a048528bca3e51d8bbb0777b8b6a130f4f53d7f
Denis Kyashif <denis.kyashif@gmail.com>
2017-02-05 10:38:14 +0200

    added tests for the english stemmer

---
+++ src/RankedSearch.Tests/EnglishStemmerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Iveonik.Stemmers;
+
+namespace RankedSearch.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class EnglishStemmerTests
+    {
+        [TestMethod]
+        public void InvokingTheConstructor_WithNoArguments_ShouldReturnNewInstance()
+        {
+            var stemmer = new EnglishStemmer();
+            Assert.IsInstanceOfType(stemmer, typeof(EnglishStemmer));
+        }
+
+        [TestMethod]
+        public void Stemming_TheWordReading_ShouldReturnRead()
+        {
+            var stemmer = new EnglishStemmer();
+            var actual = stemmer.Stem("reading");
+            Assert.AreEqual("read", actual);
+        }
+
+        [TestMethod]
+        public void Stemming_TheWordCriticized_ShouldReturnCritic()
+        {
+            var stemmer = new EnglishStemmer();
+            var actual = stemmer.Stem("criticized");
+            Assert.AreEqual("critic", actual);
+        }
+
+        [TestMethod]
+        public void Stemming_TheWordMoisturize_ShouldReturnMoistur()
+        {
+            var stemmer = new EnglishStemmer();
+            var actual = stemmer.Stem("moisturize");
+            Assert.AreEqual("moistur", actual);
+        }
+    }
+}
